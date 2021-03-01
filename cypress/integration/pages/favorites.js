4d85deaee79e0423a67dc763099db7ac7839400b
Shahid Shahidul <MdSiY@Shahids-MacBook-Pro.local>
2021-03-01 15:24:46 -0500

    first update

--- cypress/integration/pages/favorites.js
+++ cypress/integration/pages/favorites.js
@@ -6,6 +6,7 @@ export {favorites}
 
 const favorites = {
   favoriteTeam: '//div[@class="mlb-favorites-team-name" and text()="Baltimore Orioles"]',
+  favoriteTeam2:'//div[@class="mlb-favorites-team-name" and text()="Cleveland Indians"]',
   next: 'button[aria-label^="Next"]',
   done: 'button[aria-label^="Done"]',
   continue: '.mlb-onboarding-tile-label'
